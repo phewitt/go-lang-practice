f9e80bbf8268da160d31faf5288c179430e7f6cd
Parker Hewitt <Hewittparker@gmail.com>
2017-12-05 23:41:37 -0700

    add templates, err handling, and new handlers

--- gowiki/page.go
+++ gowiki/page.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
-	"net/http"
 )
 
 type Page struct {
@@ -23,10 +21,4 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
